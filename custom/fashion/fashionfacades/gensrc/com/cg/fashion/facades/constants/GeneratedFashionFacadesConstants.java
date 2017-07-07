2f2a3febe1553ba82d89ff38b9246674d436e041
ram0996 <venkat05.hybris@gmail.com>
2017-07-07 21:55:37 +0530

    Hybris 6.3 initial data
    Just start

---
+++ custom/fashion/fashionfacades/gensrc/com/cg/fashion/facades/constants/GeneratedFashionFacadesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jul 1, 2017 9:49:17 AM                      ---
+ * ----------------------------------------------------------------
+ */
+package com.cg.fashion.facades.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedFashionFacadesConstants
+{
+	public static final String EXTENSIONNAME = "fashionfacades";
+	
+	protected GeneratedFashionFacadesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ custom/fashion/fashioninitialdata/gensrc/com/cg/fashion/initialdata/constants/GeneratedFashionInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jul 1, 2017 9:49:17 AM                      ---
+ * ----------------------------------------------------------------
+ */
+package com.cg.fashion.initialdata.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedFashionInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "fashioninitialdata";
+	
+	protected GeneratedFashionInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
