f23ac7e56d3d4395890b1ead86b3f61210be431e
ram0996 <venkat05.hybris@gmail.com>
2017-07-08 09:06:10 +0530

    code commit

---
+++ custom/fashionfacades/gensrc/com/cg/fashion/facades/constants/GeneratedFashionFacadesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jul 8, 2017 8:58:09 AM                      ---
+ * ----------------------------------------------------------------
+ */
+package com.cg.fashion.facades.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedFashionFacadesConstants
+{
+	public static final String EXTENSIONNAME = "fashionfacades";
+	
+	protected GeneratedFashionFacadesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
